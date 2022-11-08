3c032d1a540d55bbd09c2121991736147b55e1f5
InkVizitor32 <morgunov.roman89@gmail.com>
2022-11-08 22:26:59 +0300

    docs: 21hw

--- Program.cs
+++ Program.cs
@@ -17,3 +17,30 @@
 
 // if (n1==n5 && n2==n4) Console.WriteLine("да");
 // else Console.WriteLine("нет");
+
+
+//                                          Задача 21
+
+//                     Напишите программу, которая принимает на вход координаты
+//                     двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Clear();
+Console.WriteLine("Введите координаты x точки A");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты y точки A");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты z точки A");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты x точки B");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты y точки B");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты z точки B");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+                Console.WriteLine($"Расстояние между двумя точками в пространстве равно {distance}");
+Console.WriteLine($"Расстояние между двумя точками равно {distance}");
